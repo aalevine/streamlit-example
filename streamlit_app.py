5e31fd461cab707f9afe148b3780924a01177a18
aalevine <40926913+aalevine@users.noreply.github.com>
2022-01-31 19:00:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import requests
 
 with st.echo(code_location='below'):     
         
-    def update_column(operation, payload):
+    def patch_property(operation, payload):
         
         url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
 
@@ -40,7 +40,7 @@ with st.echo(code_location='below'):
         
         for task in tasks:
             operation, payload = task
-            update_column(operation, payload)
+            patch_property(operation, payload)
         
         
     st.button("Clear daily checklist", on_click=clear_daily_checklist)
