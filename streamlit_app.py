6497f4f084812492907faeb20c8174447b1220aa
aalevine <40926913+aalevine@users.noreply.github.com>
2022-01-31 09:59:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,19 +16,20 @@ In the meantime, below is an example of what you can do with just a few lines of
 with st.echo(code_location='below'):
     
     def clear_daily():
-        url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
+        print("test")
+#         url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
 
-        payload = {"properties": {"Done": {"title": [{"checkbox": {"content": 0}}]}}}
-        print(payload)
-        headers = {
-            "Accept": "application/json",
-            "Notion-Version": "2021-08-16",
-            "Content-Type": "application/json",
-            "Authorization": "Bearer secret_bbkXQFbGmg9G8hqXxzPPc4QMwAv1mmFQgoRDfSC7jfx"
-        }
+#         payload = {"properties": {"Done": {"title": [{"checkbox": {"content": 0}}]}}}
+#         print(payload)
+#         headers = {
+#             "Accept": "application/json",
+#             "Notion-Version": "2021-08-16",
+#             "Content-Type": "application/json",
+#             "Authorization": "Bearer secret_bbkXQFbGmg9G8hqXxzPPc4QMwAv1mmFQgoRDfSC7jfx"
+#         }
 
-        response = requests.request("PATCH", url, json=payload, headers=headers)
+#         response = requests.request("PATCH", url, json=payload, headers=headers)
 
-        print(response.text)        
+#         print(response.text)        
         
     st.button("Clear daily checklist", on_click=clear_daily())
