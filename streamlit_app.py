ea51162cd907c097a5121ebe2f0441b2c1cba29e
aalevine <40926913+aalevine@users.noreply.github.com>
2022-01-31 18:56:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,10 @@ import requests
 
 
 with st.echo(code_location='below'):     
-
-    def delete_done_column():
+        
+    def update_column(operation, payload):
         
         url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
-        
-        payload = {"properties":{"Done": None}}
 
         headers = {
             "Accept": "application/json",
@@ -22,32 +20,27 @@ with st.echo(code_location='below'):
         }
 
         response = requests.request("PATCH", url, json=payload, headers=headers)        
-        st.write("Deleted 'Done' column.")
+
+        st.write("{}d 'Done' column.".format(operation))              
         st.write(response.text)
         
         
-    def create_done_column():
+    def clear_daily_checklist():
         
-        url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
+        tasks = [
+            (
+                "delete",
+                payload = {"properties":{"Done": None}}
+            ),
+            (
+                "create",
+                payload = {"properties":{"Done":{"checkbox":{}}}}                 
+            )
+        ]
         
-        payload = {"properties":{"Done":{"checkbox":{}}}}
-
-        headers = {
-            "Accept": "application/json",
-            "Notion-Version": "2021-08-16",
-            "Content-Type": "application/json",
-            "Authorization": "Bearer secret_mduk29ERIsnmXXmLrkwdy11c0G2DVdm9K2LTqohkBDH"
-        }
-
-        response = requests.request("PATCH", url, json=payload, headers=headers)        
-
-        st.write("Recreated 'Done' column.")              
-        st.write(response.text)      
-        
-        
-    def clear_daily():
-        delete_done_column()
-        create_done_column()
+        for task in tasks:
+            operation, payload = task
+            update_column(operation, payload)
         
         
-    st.button("Clear daily checklist", on_click=clear_daily)
+    st.button("Clear daily checklist", on_click=clear_daily_checklist)
