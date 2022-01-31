aab5c94ca801b141c6f78e5b1474e81f80136133
aalevine <40926913+aalevine@users.noreply.github.com>
2022-01-31 18:37:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import requests
 
 with st.echo(code_location='below'):     
 
-    def clear_daily():
+    def delete_done_column():
         
         url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
         
@@ -24,5 +24,29 @@ with st.echo(code_location='below'):
         response = requests.request("PATCH", url, json=payload, headers=headers)        
 
         st.write(response.text)
+        
+        
+    def create_done_column():
+        
+        url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
+        
+        payload = {"properties":{"Done":{"checkbox":{}}}}
+
+        headers = {
+            "Accept": "application/json",
+            "Notion-Version": "2021-08-16",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer secret_mduk29ERIsnmXXmLrkwdy11c0G2DVdm9K2LTqohkBDH"
+        }
+
+        response = requests.request("PATCH", url, json=payload, headers=headers)        
+
+        st.write(response.text)      
+        
+        
+    def clear_daily()
+        delete_done_column()
+        create_done_column()
+        
         
     st.button("Clear daily checklist", on_click=clear_daily)
