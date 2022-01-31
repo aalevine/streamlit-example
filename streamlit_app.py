ef4f8fa2096c66217e927abd6a30d4fdbe26883d
aalevine <40926913+aalevine@users.noreply.github.com>
2022-01-31 09:35:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,12 @@ with st.echo(code_location='below'):
     def clear_daily():
         url = "https://api.notion.com/v1/databases/496f4943fe29407098c94d5b7b4e8ce4"
 
-        payload = {"properties": {"Done": {"title": [{"checkbox": {"content": 1}}]}}}
+        payload = {"properties": {"Done": {"title": [{"checkbox": {"content": 0}}]}}}
         headers = {
             "Accept": "application/json",
             "Notion-Version": "2021-08-16",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "Bearer secret_bbkXQFbGmg9G8hqXxzPPc4QMwAv1mmFQgoRDfSC7jfx"
         }
 
         response = requests.request("PATCH", url, json=payload, headers=headers)
